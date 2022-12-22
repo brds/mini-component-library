6e778a3d26a583bcf538dc112f0247160858b8e2
RPB <renaudpbordes@gmail.com>
2022-12-22 01:06:53 +0100

    Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,14 +5,68 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const iconSize = 24;
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange} aria-label={label}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <InlineIcon id="chevron-down" size={iconSize} strokeWidth={1} />
+      </PresentationalBit>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: fit-content;
+`;
+
+const NativeSelect = styled.select`
+  opacity: 0;
+  position: absolute;
+  inset: 0;
+  /** Remove Safari restriction on styling the height of select */
+  appearance: none;
+`;
+
+const iconRightSpace = 10;
+const iconLeftSpace = 10;
+
+const PresentationalBit = styled.div`
+  font-family: 'Roboto', sans-serif;
+  font-size: ${16 / 16}rem;
+  line-height: ${19 / 16}rem;
+  color: ${COLORS.gray700};
+  padding: 12px calc(${iconLeftSpace}px + ${iconSize}px + ${iconRightSpace}px) 12px 16px;
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+
+  ${NativeSelect}:focus + & {
+    outline: 2px solid black;
+    outline: 2px auto -webkit-focus-ring-color;
+  }
+`;
+
+const InlineIcon = styled(Icon)`
+  width: ${iconSize}px;
+  height: ${iconSize}px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: ${iconRightSpace}px;
+  margin-block: auto;
+  pointer-events: none;
+`
 
 export default Select;
